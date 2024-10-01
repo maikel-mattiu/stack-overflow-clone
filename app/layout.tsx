dbdd85fa13606b741b0bfabca97f3d2757cd02f0
maikel-mattiu <maikmatt@outlook.com>
2024-10-01 12:18:05 +0100

    app layout modified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,53 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import "./globals.css"
-import React from "react"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next'
+import './globals.css'
+import React from 'react'
 
-export const metadata = {
-	title: "DevFlow"
+export const metadata: Metadata = {
+  title: 'DevFlow',
+  description: 'A Community driven platform for developers',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<ClerkProvider>
-			<html lang="en">
-				<body>{children}</body>
-			</html>
-		</ClerkProvider>
-	)
+const inter = Inter({ subsets: ['latin'],weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], variable: '--font-inter' })
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'], variable: '--font-space-grotesk' })
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 
+          'primary-text-gradient hover:text-primary-500',
+        }
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
