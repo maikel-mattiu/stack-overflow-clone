760be12430998998b6dd765af5b4a5b48be92fcd
maikel-mattiu <maikmatt@outlook.com>
2024-10-05 07:43:38 +0100

    Right Sidebar UI Implemented

---
+++ components/shared/sidebars/RightSideBar.tsx
@@ -0,0 +1,79 @@
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+import RenderTag from "../RenderTag"
+
+const RightSideBar = () => {
+	const hotQuestions = [
+		{
+			_id: 1,
+			title: "How to center a div?"
+		},
+		{
+			_id: 2,
+			title: "What is the difference between let, const, and var?"
+		},
+		{
+			_id: 3,
+			title: "How to fetch data from an API in React?"
+		},
+		{
+			_id: 4,
+			title: "What does '.gitignore' file do?"
+		}
+	]
+
+	const popularTags = [
+		{ _id: 1, name: "javascript", totalQuestions: 10 },
+		{ _id: 2, name: "react", totalQuestions: 5 },
+		{ _id: 3, name: "nextjs", totalQuestions: 53 },
+		{ _id: 4, name: "node", totalQuestions: 72 },
+		{ _id: 5, name: "express", totalQuestions: 91 },
+		{ _id: 6, name: "mongodb", totalQuestions: 12 },
+		{ _id: 7, name: "python", totalQuestions: 60 }
+	]
+
+	return (
+		<section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+			<div>
+				<h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+				<div className="mt-7 flex w-full flex-col gap-[30px]">
+					{hotQuestions.map((question) => (
+						<Link
+							href={`/questions/${question._id}`}
+							key={question._id}
+							className="flex cursor-pointer items-center justify-between gap-7"
+						>
+							<p className="body-medium text-dark500_light700">
+								{question.title}
+							</p>
+							<Image
+								src={"/assets/icons/chevron-right.svg"}
+								alt={"chevron-right"}
+								width={20}
+								height={20}
+								className={"invert-colors"}
+							/>
+						</Link>
+					))}
+				</div>
+			</div>
+			<div className="mt-16">
+				<h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+				<div className="mt-7 flex flex-col gap-4">
+					{popularTags.map((tag) => (
+						<RenderTag
+							key={tag._id}
+							id={tag._id}
+							name={tag.name}
+							totalQuestions={tag.totalQuestions}
+							showCount={true}
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default RightSideBar
