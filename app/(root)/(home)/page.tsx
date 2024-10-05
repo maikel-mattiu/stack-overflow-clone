c1fa811cdd8ad7784919ed9e42cf3206689b3de5
maikel-mattiu <maikmatt@outlook.com>
2024-10-05 09:01:20 +0100

    Home UI v1

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,31 @@
-// import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-// import { useTheme } from "@/context/ThemeProvider"
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Home() {
-	// const { theme } = useTheme()
-	// console.log(theme)
 	return (
 		<>
-			<p>Home</p>
+			<div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+				<h1 className="h1-bold text-dark100_light900">All Questions</h1>
+				<Link
+					href="/ask-questions"
+					className="flex justify-end max-sm:w-full"
+				>
+					<Button className="primary-gradient min-h-[46px] rounded-lg px-4 py-3 !text-light-900">
+						Ask a Questions
+					</Button>
+				</Link>
+			</div>
+
+			<div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+				<LocalSearchBar
+					route="/"
+					position=""
+					src=""
+					placeholder=""
+					otherClasses={""}
+				/>
+			</div>
 		</>
 	)
 }
