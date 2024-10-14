b58c424657222f624f8e258064333b5b27ee550d
maikel-mattiu <maikmatt@outlook.com>
2024-10-14 09:21:49 +0100

    webhook middleware modified

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/api/(.*)', 'question/:id', '/tags', '/tags/:id', '/profile/:id', '/community', 'jobs'])
-const ignoredRoutes = ['/api/webhook', '/api/chatgpt']
-
+const ignoredRoutes = ['/api/webhook', '/api/chatgpt', '/your-new-ignored-route'] // Add your new route here
 export default clerkMiddleware((auth, request) => {
+  // Check if the route is in the ignoredRoutes array
+  if (ignoredRoutes.some(route => request.url.includes(route))) {
+    return; // Skip middleware for ignored routes
+  }
+
   if (!isPublicRoute(request)) {
     auth().protect()
   }
-  if (ignoredRoutes.includes(request.nextUrl.pathname)) {
-    // eslint-disable-next-line no-useless-return
-    return
-  }
 });
-
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
